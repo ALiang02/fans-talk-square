1a586c5be400a14935af53c77c11b6caf0a875fd
ALiang02 <1745346447@qq.com>
2020-02-29 21:04:56 +0800

    导航完成

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <el-container>
+      <el-aside width="200px">
+        <hello-world />
+      </el-aside>
+      <el-main>
+        <square-center />
+      </el-main>
+    </el-container>-->
+
+    <el-row>
+      <el-col :span="4">
+        <SquareGuid />
+      </el-col>
+      <el-col :span="20">
+        <router-view />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import SquareGuid from "./components/SquareGuid.vue";
+
+export default {
+  name: "App",
+  components: {
+    SquareGuid
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/SquareGuid.vue
@@ -0,0 +1,38 @@
+<template >
+  <div>
+    <el-row class="tac">
+      <el-col :span="12">
+        <el-menu default-active="1" class="el-menu-vertical-demo" router>
+          <el-menu-item index="SquareCenter">
+            <i class="el-icon-location"></i>
+            <span slot="title">广场</span>
+          </el-menu-item>
+          <el-menu-item index="PersonalCenter">
+            <i class="el-icon-user"></i>
+            <span slot="title">个人</span>
+          </el-menu-item>
+          <div id="menu-space"></div>
+          <el-menu-item index="PersonalSetting">
+            <i class="el-icon-setting"></i>
+            <span slot="title">设置</span>
+          </el-menu-item>
+          <el-menu-item index="SquareInfo">
+            <i class="el-icon-info"></i>
+            <span slot="title">关于</span>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style css>
+#menu-space {
+  margin: 680px;
+}
+</style>
+
+
